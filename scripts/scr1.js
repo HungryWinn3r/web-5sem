01c3016319c1f66142074dd8f990a0fad2772c18
HungryWinn3r <greezle.gg@mail.ru>
2022-12-20 20:45:19 +0300

    feat: lab-4

---
+++ scripts/scr1.js
@@ -0,0 +1,14 @@
+(function () {
+  document.addEventListener("DOMContentLoaded", domLoadTime);
+})();
+
+function domLoadTime() {
+  window.addEventListener("load", loadTime);
+  var time = new Date().getTime();
+  const h6 = document.querySelector("h6");
+  function loadTime() {
+    h6.innerHTML = new Date().getTime() - time;
+  }
+}
+
+// requestAnimationFrame(domLoadTime);
